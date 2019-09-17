991120beb671fda6b465ae645d6eb7dfbff8b11f
aharshac <aharshac@gmail.com>
2019-09-17 22:20:45 +0530

    Enable junction deviation on all

--- src/Marlin/Configuration_adv.h
+++ src/Marlin/Configuration_adv.h
@@ -81,6 +81,11 @@
 
 // #define LIN_ADVANCE
 
+#define JUNCTION_DEVIATION
+#if ENABLED(JUNCTION_DEVIATION)
+  #define JUNCTION_DEVIATION_MM 0.05  // (mm) Distance from real junction edge
+#endif
+
 //===========================================================================
 //===================== End of Fracktal Works Julia 2018 ====================
 //===========================================================================
@@ -498,9 +503,9 @@
 // Use Junction Deviation instead of traditional Jerk Limiting
 //
 //#define JUNCTION_DEVIATION
-#if ENABLED(JUNCTION_DEVIATION)
-  #define JUNCTION_DEVIATION_MM 0.02  // (mm) Distance from real junction edge
-#endif
+// #if ENABLED(JUNCTION_DEVIATION)
+//   #define JUNCTION_DEVIATION_MM 0.02  // (mm) Distance from real junction edge
+// #endif
 
 /**
  * Adaptive Step Smoothing increases the resolution of multi-axis moves, particularly at step frequencies
