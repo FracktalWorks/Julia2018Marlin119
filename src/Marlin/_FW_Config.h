076467138348f1efc604145eb8aa1add71063930
Vijay Raghav Varada <vjvarada@yahoo.co.in>
2020-03-02 18:30:19 +0530

    Removed S curve accleleration since it was causing unwanted banding in Z

--- src/Marlin/_FW_Config.h
+++ src/Marlin/_FW_Config.h
@@ -146,7 +146,7 @@
 #define HOMING_FEEDRATE_Z   (20*60)
 
 /**  Movement  **/
-#define S_CURVE_ACCELERATION
+//#define S_CURVE_ACCELERATION
 #if BV_PRO_ABL24()
   //#define DEFAULT_AXIS_STEPS_PER_UNIT   { 200,  200, 1007.874, 280 }  for 1/32 microstep ratio
   #define DEFAULT_AXIS_STEPS_PER_UNIT   { 100,  100, 503.937, 140 }
