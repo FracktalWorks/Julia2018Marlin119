35847bd0235bfbff34327f0a23b93374e85660b1
ganeshmev <35722311+ganeshmev@users.noreply.github.com>
2020-09-23 13:50:19 +0530

    Update Configuration.h

--- src/Marlin/Configuration.h
+++ src/Marlin/Configuration.h
@@ -347,8 +347,8 @@
 // You should use MINTEMP for thermistor short/failure protection.
 #define HEATER_0_MAXTEMP 350
 #define HEATER_1_MAXTEMP 350
-#define HEATER_2_MAXTEMP 120  //chamber heater
-#define HEATER_3_MAXTEMP 60  //Filament heater
+#define HEATER_2_MAXTEMP 120  //Chamber heater or Build volume heater
+#define HEATER_3_MAXTEMP 60  //Filament chamber heater
 //#define HEATER_4_MAXTEMP 275
 #define BED_MAXTEMP 150
 
